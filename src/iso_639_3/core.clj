1fcb4086ac856393b969b9f9995db0a9e2a34353
max thomas <max@maxthomas.io>
2016-05-11 18:58:13 -0400

    Initial check in.

---
+++ src/iso_639_3/core.clj
@@ -0,0 +1,31 @@
+(ns iso-639-3.core
+  (:gen-class)
+  (:require [org.httpkit.client :as http]
+            [cheshire.core :as json]))
+
+(def iso-url "http://www-01.sil.org/iso639-3/iso-639-3.tab")
+
+(defn get-data []
+  (let [{:keys [status headers body error] :as resp} @(http/get iso-url)]
+    (if error
+      (println "Failed, exception: " error)
+      body)))
+
+(defn tab-json [^String tabs]
+  (->> tabs
+       (clojure.string/split-lines)
+       (drop 1)
+       (map #(let [spl (clojure.string/split % #"\t")]
+               {:code (first spl)
+                :name (last spl)}))))
+
+(defn as-json [^String fn]
+  (let [results (->> (get-data)
+                     tab-json
+                     json/encode)]
+    (spit fn results)))
+
+(defn -main
+  "Create a file named 'iso-639-3.json' with the latest codes."
+  [& args]
+  (as-json "iso-639-3.json"))
